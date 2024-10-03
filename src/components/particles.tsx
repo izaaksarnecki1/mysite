422c2695f59f7cc181641063e80d07523bcc741f
Izaak Sarnecki <izaaksarnecki@outlook.com>
2024-10-03 15:56:31 +0200

    format

--- src/components/particles.tsx
+++ src/components/particles.tsx
@@ -8,25 +8,24 @@ export const Particles = () => {
 
   return (
     <div className="absolute inset-0 overflow-hidden z-0">
-    {[...Array(50)].map((_, i) => (
-      <motion.div
-        key={i}
-        className="w-2 h-2 rounded-full bg-[hsl(var(--accent))] absolute"
-        style={{
-          top: `${Math.random() * 100}%`,
-          left: `${Math.random() * 100}%`,
-        }}
-        initial="initial"
-        animate="animate"
-        variants={particleVariants}
-        transition={{
-          duration: Math.random() * 5 + 2,
-          repeat: Infinity,
-          repeatType: "loop",
-        }}
-      />
-    ))}
-  </div>
-);
-
+      {[...Array(50)].map((_, i) => (
+        <motion.div
+          key={i}
+          className="w-2 h-2 rounded-full bg-[hsl(var(--accent))] absolute"
+          style={{
+            top: `${Math.random() * 100}%`,
+            left: `${Math.random() * 100}%`,
+          }}
+          initial="initial"
+          animate="animate"
+          variants={particleVariants}
+          transition={{
+            duration: Math.random() * 5 + 2,
+            repeat: Infinity,
+            repeatType: "loop",
+          }}
+        />
+      ))}
+    </div>
+  );
 };
